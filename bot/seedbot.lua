ba46c59fd96e1028e824034205bf402ab38df4dc
tahashakiba <m.tahashakiba@gmail.com>
2016-02-14 16:08:41 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {195473011,126712741},--Sudo users
+    sudo_users = {195473011,126712741,133246179},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
